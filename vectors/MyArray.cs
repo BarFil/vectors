0bc656e2d46c0c8f71769949d0dc80af99124f51
mbara <mbara@DESKTOP-V49383C>
2022-05-18 15:49:12 +0200

    Dodaj pliki projektów.

---
+++ vectors/MyArray.cs
@@ -0,0 +1,75 @@
+﻿namespace vectors
+{
+    class MyArray<Type>
+    {
+        private Type[] myArray;
+        public event EventHandler<int> SizeChanged;
+        public event EventHandler<int> AllocatedMemoryChanged;
+
+        protected virtual void OnSizeChanged(int newSize)
+        {
+            SizeChanged?.Invoke(this, newSize);
+        }
+        protected virtual void OnAllocatedMemoryChanged(int newSize)
+        {
+            AllocatedMemoryChanged?.Invoke(this, newSize);
+        }
+        public int Size
+        {
+            get;
+            private set;
+        }
+        public Type this[int index]
+        {
+            get
+            {
+                if (index >= Size || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("", message: "Wartość o danym indeksie jest nieosiągalna: " + index);
+                }
+                return myArray[index];
+            }
+            set
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("", message: "Wartość o danym indeksie jest nieosiągalna: " + index);
+                }
+                if (index >= myArray.Length)
+                {
+                    Array.Resize<Type>(ref myArray, (index + 1) * 2);
+                    OnAllocatedMemoryChanged(myArray.Length);
+                }
+                if (index >= Size)
+                {
+                    Size = index + 1;
+                    OnSizeChanged(Size);
+                }
+                myArray[index] = value;
+            }
+
+        }
+
+        public MyArray(int capacity = 0)
+        {
+            myArray = new Type[capacity];
+            Size = 0;
+        }
+
+        public void Add(Type value)
+        {
+            this[Size] = value;
+        }
+
+        public void Dump()
+        {
+            Console.WriteLine("Allocated memory: " + myArray.Length);
+            Console.WriteLine("Current size: " + Size);
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write(myArray[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ vectors/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace vectors
+{
+    class Program
+    {
+        public static void tableSizeChanged(object sender, int newSize)
+        {
+            Console.WriteLine("New Size: " + newSize);
+        }
+        public static void tableMemoryChanged(object sender, int newSize)
+        {
+            Console.WriteLine("New allocated memory: " + newSize);
+        }
+        static void Main()
+        {
+            MyArray<int> tablica = new MyArray<int>(0);
+            tablica.SizeChanged += tableSizeChanged;
+            tablica.AllocatedMemoryChanged += tableMemoryChanged;
+            tablica[0] = 1;
+            tablica.Dump();
+            Console.WriteLine("");
+
+            tablica[3] = 2;
+            tablica.Dump();
+            Console.WriteLine("");
+
+            tablica[2] = 3;
+            tablica.Dump();
+            Console.WriteLine("");
+
+            tablica.Add(1);
+            tablica.Dump();
+            Console.WriteLine("");
+
+            tablica[7] = 9;
+            tablica.Dump();
+            Console.WriteLine("");
+
+            tablica.Add(2);
+            tablica.Dump();
+            Console.WriteLine("");
+
+            try
+            {
+                //tablica[-1] = 3;
+                Console.WriteLine(tablica[44]);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
